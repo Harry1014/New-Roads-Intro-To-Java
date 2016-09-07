01c15b782fe792d9d640929ac90c738487f18546
hschlossberg <hschlossberg@newroads.com>
2016-09-07 12:29:06 -0400

    Added Average class

---
+++ Average.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Average{
+public static void main(String[] args){
+  
+  
+  /*int x = 3;
+  ddouble number = 3.0;
+  sting word ="turkey"
+  char letter = 't'
+    */
+  
+  
+  Scanner input = new Scanner (system.in);
+  
+    system.out.printing ("enter 3 numbers:");
+    
+  double num1 = input.nextDouble();
+  double num2 = input.nextDouble();
+  double num3 = input.nextDouble();
+  
+  double average = (num1 + num2 + num3)/3;
+  System.out.println("this is the average" + average);
+    
+    
+    
+}
+
+
+
+}
